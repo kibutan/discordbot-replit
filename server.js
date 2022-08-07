280573891ea38b5b025194b2572e062f515905db
木伏 徹 <46431930+kibutan@users.noreply.github.com>
2022-08-07 15:05:28 +0000

    postbody

--- server.js
+++ server.js
@@ -1,4 +1,30 @@
 //reference https://note.com/suzuneu/n/n80799a0f39d9
+const Holidays=[
+  ["2022/09/03"],
+  ["2022/09/04"],
+  ["2022/09/10"],
+  ["2022/09/11"],
+  ["2022/09/17"],
+  ["2022/09/18"],
+  ["2022/09/19"],
+  ["2022/09/23"],
+  ["2022/09/24"],
+  ["2022/09/25"],
+]
+
+const Reactions =[
+  ["1⃣"],
+  ["2⃣"],
+  ["3⃣"],
+  ["4⃣"],
+  ["5⃣"],
+  ["6⃣"],
+  ["7⃣"],
+  ["8⃣"],
+  ["9⃣"],
+  ["🇦"],
+]
+
 const http = require("http");
 const querystring = require("querystring");
 const {
@@ -47,9 +73,23 @@ client.on("messageCreate", async (message) => {
     }  
 });
 
-cron.schedule("* * * * *", async () => {
+function MakePostBody(){
+  const holidaysLength = Holidays.length
+  let body = ""
+  for(let i = 0; i < holidaysLength ; i++){
+    body += Reactions[i]+ ":"+ Holidays[i] +"\n"
+    console.log("Post body"+body+Reactions[i] + Holidays[i])
+  }
+  return body
+}
+
+const postBody = MakePostBody()
+
+cron.schedule("* 0 * * *", async () => {
+  const holidaysLength = Holidays.length
+  console.log(postBody)
   const sendmessage = await client.channels.cache.get("834044144162308120")
-    .send("スケジュール調整！")
+    .send("スケジュール調整！\n" + postBody)
     .then(message => message.react("1⃣")
       .then(() => message.react("2⃣"))
       .then(() => message.react("3⃣"))
@@ -59,12 +99,12 @@ cron.schedule("* * * * *", async () => {
       .then(() => message.react("7⃣"))
       .then(() => message.react("8⃣"))
       .then(() => message.react("9⃣"))
-      // .then(() => message.react("🇦"))
-      // .then(() => message.react("🇧"))
-      // .then(() => message.react("🇨"))
-      // .then(() => message.react("🇩"))
-      // .then(() => message.react("🇪"))
-      // .then(() => message.react("🇫"))
+      .then(() => message.react("🇦"))
+      .then(() => message.react("🇧"))
+      .then(() => message.react("🇨"))
+      .then(() => message.react("🇩"))
+      .then(() => message.react("🇪"))
+      .then(() => message.react("🇫"))
       // .then(() => message.react("🇬"))
       // .then(() => message.react("🇭"))
       // .then(() => message.react("🇮"))
