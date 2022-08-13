e6d538581a435719df795ecc7b71d806d405641b
木伏 徹 <46431930+kibutan@users.noreply.github.com>
2022-08-13 13:32:18 +0000

    prepare for AmongUs Server

--- server.js
+++ server.js
@@ -1,18 +1,5 @@
 //reference https://note.com/suzuneu/n/n80799a0f39d9
-const Holidays=[
-  ["2022/09/03"],
-  ["2022/09/04"],
-  ["2022/09/10"],
-  ["2022/09/11"],
-  ["2022/09/17"],
-  ["2022/09/18"],
-  ["2022/09/19"],
-  ["2022/09/23"],
-  ["2022/09/24"],
-  ["2022/09/25"],
-]
-
-const Reactions =[
+const Reactions = [
   ["1⃣"],
   ["2⃣"],
   ["3⃣"],
@@ -23,7 +10,11 @@ const Reactions =[
   ["8⃣"],
   ["9⃣"],
   ["🇦"],
-]
+  ["🇧"],
+  ["🇨"],
+  ["🇩"],
+  ["🇪"],
+];
 
 const http = require("http");
 const querystring = require("querystring");
@@ -50,86 +41,73 @@ async function getJSONResponse(body) {
 }
 
 try {
- // GAS(Google Apps Script)からの受信(botの常時起動)
- http.createServer(function(req, res){
-  res.write("OK");
-  res.end();
- }).listen(8080);
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
+  // GAS(Google Apps Script)からの受信(botの常時起動)
+  http
+    .createServer(function (req, res) {
+      res.write("OK");
+      res.end();
+    })
+    .listen(8080);
+  client.on("ready", () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+  });
 
-client.on("messageCreate", async (message) => {
-    if(message.content==="pong"){
-      message.reply("教えはどうなってんだ教えは")
+  client.on("messageCreate", async (message) => {
+    if (message.content === "pong") {
+      message.reply("Pong" + postBody);
     }
-    if(message.content==="holiday"){
-      // message.reply(")")
+  });
+
+  async function MakePostBody() {
     const holidayResult = await request(
-"https://sheets.googleapis.com/v4/spreadsheets/1saWrdjSqAp9P09cFQ0VgTMbYAjLwVnreft44vvM1mVg/values/allholiday?key="+process.env.GOOGLE_API_KEY
+      "https://sheets.googleapis.com/v4/spreadsheets/1saWrdjSqAp9P09cFQ0VgTMbYAjLwVnreft44vvM1mVg/values/NextMonthHolidays?key=" +
+        process.env.GOOGLE_API_KEY
     );
     const { values } = await getJSONResponse(holidayResult.body);
-    console.log(values[0]);
-    }  
-});
-
-function MakePostBody(){
-  const holidaysLength = Holidays.length
-  let body = ""
-  for(let i = 0; i < holidaysLength ; i++){
-    body += Reactions[i]+ ":"+ Holidays[i] +"\n"
-    console.log("Post body"+body+Reactions[i] + Holidays[i])
+    const holidaysLength = values.length;
+    let body = "";
+    for (let i = 0; i < holidaysLength; i++) {
+      body += Reactions[i] + ":" + values[i] + "\n";
+      console.log("Post body" + body + Reactions[i] + values[i]);
+    }
+    console.log(body);
+    return body;
   }
-  return body
-}
 
-const postBody = MakePostBody()
+  let postBody;
+  MakePostBody().then((body) => {
+    console.log("postBody in ", body);
+    postBody = body;
+  });
 
-cron.schedule("* 0 * * *", async () => {
-  const holidaysLength = Holidays.length
-  console.log(postBody)
-  const sendmessage = await client.channels.cache.get("834044144162308120")
-    .send("スケジュール調整！\n" + postBody)
-    .then(message => message.react("1⃣")
-      .then(() => message.react("2⃣"))
-      .then(() => message.react("3⃣"))
-      .then(() => message.react("4⃣"))
-      .then(() => message.react("5⃣"))
-      .then(() => message.react("6⃣"))
-      .then(() => message.react("7⃣"))
-      .then(() => message.react("8⃣"))
-      .then(() => message.react("9⃣"))
-      .then(() => message.react("🇦"))
-      .then(() => message.react("🇧"))
-      .then(() => message.react("🇨"))
-      .then(() => message.react("🇩"))
-      .then(() => message.react("🇪"))
-      .then(() => message.react("🇫"))
-      // .then(() => message.react("🇬"))
-      // .then(() => message.react("🇭"))
-      // .then(() => message.react("🇮"))
-      // .then(() => message.react("🇯"))
-      // .then(() => message.react("🇱"))
-      // .then(() => message.react("🇲"))
-      // .then(() => message.react("🇳"))
-      // .then(() => message.react("🇴"))
-      // .then(() => message.react("🇵"))
-      // .then(() => message.react("🇶"))
-      // .then(() => message.react("🇷"))
-      // .then(() => message.react("🇸"))
-      // .then(() => message.react("🇹"))
-      // .then(() => message.react("🇺"))
-      // .then(() => message.react("🇻"))
-      // .then(() => message.react("🇼"))
-      // .then(() => message.react("🇽"))
-      // .then(() => message.react("🇾"))
-      // .then(() => message.react("🇿"))
-    )
-  
-});
-  
-client.login(process.env.DISCORD_BOT_TOKEN);
+  cron.schedule("0 12 20 * *", async () => {
+    // const holidaysLength = Holidays.length;
+    // console.log(postBody);
+    const sendmessage = await client.channels.cache
+      .get("851072678000328716")
+      .send("スケジュール調整！\n" + postBody + "\n ❌:全部無理")
+      .then(
+        (message) =>
+          message
+            .react("1⃣")
+            .then(() => message.react("2⃣"))
+            .then(() => message.react("3⃣"))
+            .then(() => message.react("4⃣"))
+            .then(() => message.react("5⃣"))
+            .then(() => message.react("6⃣"))
+            .then(() => message.react("7⃣"))
+            .then(() => message.react("8⃣"))
+            .then(() => message.react("9⃣"))
+            .then(() => message.react("🇦"))
+            .then(() => message.react("🇧"))
+            .then(() => message.react("🇨"))
+            .then(() => message.react("🇩"))
+            .then(() => message.react("🇪"))
+            .then(() => message.react("❌"))
+      );
+  });
 
+  client.login(process.env.DISCORD_BOT_TOKEN);
 } catch (e) {
- console.log(e);
+  console.log(e);
 }
